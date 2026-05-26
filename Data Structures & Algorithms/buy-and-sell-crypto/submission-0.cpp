8978f6303a0b1188c9941162a15e838f8bd87eab
Aditya Patil <adi.mi.patil@gmail.com>
2026-05-26 14:41:29 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans=0;
+        for(int i=0;i<prices.size();i++){
+            for(int j=i+1;j<prices.size();j++){
+                int m=prices[j]-prices[i];
+                ans=max(m,ans);
+            }
+        }
+        return ans;
+    }
+};
